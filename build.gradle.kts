55df25263ab197d40de137446e52440155f54023
xuankaicat <1277961681@qq.com>
2022-02-28 15:44:13 +0800

    kotlin增加-Xmulti-platform编译参数

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
 allprojects {
     group = "com.gitee.xuankaicat"
     version = "2.0.0-dev01"
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.freeCompilerArgs += "-Xmulti-platform"
+    }
 }
