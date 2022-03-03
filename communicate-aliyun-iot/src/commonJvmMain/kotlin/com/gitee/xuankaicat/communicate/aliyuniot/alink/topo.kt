e9ed3a89f593ea5cca8a8d13eb851c986fc539c8
xuankaicat <1277961681@qq.com>
2022-03-03 13:50:06 +0800

    增加topoAdd实现添加设备拓扑关系

---
+++ communicate-aliyun-iot/src/commonJvmMain/kotlin/com/gitee/xuankaicat/communicate/aliyuniot/alink/topo.kt
@@ -0,0 +1,35 @@
+@file:Suppress("unused")
+
+package com.gitee.xuankaicat.communicate.aliyuniot.alink
+
+import com.gitee.xuankaicat.communicate.MQTTCommunicate
+import com.gitee.xuankaicat.communicate.aliyuniot.AlinkMQTT
+import com.gitee.xuankaicat.communicate.aliyuniot.AliyunMqtt
+import com.gitee.xuankaicat.communicate.aliyuniot.CreateHelper
+import com.gitee.xuankaicat.communicate.aliyuniot.utils.OnReceiveAlinkResultFunc
+import com.gitee.xuankaicat.communicate.aliyuniot.utils.sendAndReceiveAlink
+
+/**
+ * 添加设备拓扑关系
+ * @receiver MQTTCommunicate
+ */
+fun MQTTCommunicate.topoAdd(aliyunMqtt: AliyunMqtt, clientId: String? = null, onReceive: OnReceiveAlinkResultFunc) {
+    this as AlinkMQTT
+
+    val timestamp = CreateHelper.timestamp()
+    val param = mapOf(
+        "deviceName" to aliyunMqtt.deviceName,
+        "productKey" to aliyunMqtt.productKey,
+        "sign" to getSign(timestamp),
+        "signmethod" to "hmacSha256",
+        "timestamp" to timestamp,
+        "clientId" to (clientId ?: "${aliyunMqtt.productKey}&${aliyunMqtt.deviceName}"),
+    )
+
+    val msgObj = AlinkBase(nextId,
+        params = listOf(param),
+        method = "thing.topo.add"
+    )
+
+    sendAndReceiveAlink("/sys/$productKey/$deviceName/thing/topo/add", msgObj, onReceive)
+}
